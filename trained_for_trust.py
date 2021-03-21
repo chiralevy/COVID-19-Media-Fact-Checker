8dafd4bddbba1fe6199ce05c23bbe1fd12777399
jrod2699 <44716586+jrod2699@users.noreply.github.com>
2021-03-21 13:19:53 -0700

    completed task-list and provided outline for how to proceed in the project -- nothing major

---
+++ trained_for_trust.py
@@ -0,0 +1,6 @@
+# This will be the main python script
+
+# To-do: Preprocess datasets
+
+# To-do: Implement a simple NN that classifies the news articles 
+# This portion will be expanded upon later
