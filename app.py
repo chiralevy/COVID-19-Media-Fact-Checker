ac83393571d988eedba90c1a74912badd6abe225
Jorge Rodriguez <jrodriguez261099@gmail.com>
2021-05-11 16:08:42 -0700

    Added user input option to assess user-inputted covid claims in app.py

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ from keras_preprocessing.text import tokenizer_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 """
-# RCNN Fake News Classifier 
-This is a simple classifier. It takes in an article and tells you if the article is fake news or not. It was trained on a data set including articles of real and fake news. 
+# Fake News Classifier 
+This is a simple classifier. There are two options:
+1. General News: With this option, the classifier takes in an article and tells you if the article is fake news or not. It was trained on a data set including articles of real and fake news. 
+2. Covid News/Claims: With this option, the classifier takes in either text input about a covid claim or a covid-related news article url. Based on the claim or url, the classifier will tell you if the truthfulness of the claim/article. This classifier was trained on a data set including covid-related articles/claims.  
 
 """
 
@@ -102,8 +104,36 @@ def covid_predict(url, inference):
         except:
             st.text("Problem reading article/claim from", url)
             
+def covid_claim_pred(claim, inference):
+    if claim != "":
+        try:
+            content = []
+            content.append(claim)
+            
+            # Turning the claim into a data frame
+            df = pd.DataFrame(
+                             {'text': content
+                            })
             
             
+            # Pad the data similarly to how it was padded with the training data
+            sequences = tokenizer.texts_to_sequences(df['text'])
+            padded = pad_sequences(sequences, maxlen=80, padding='post', truncating='post')
+
+            # Get the model's prediction on url's news article
+            pred = inference.predict(padded)
+
+            f"""
+            # Claim: {claim}.
+            ## This claim **{'is' if pred < 0.5 else 'is not'}** False.
+            ### Probability of Truthfulness: {pred.item()*100: .2f}%
+            """
+        except:
+            st.text("Problem reading user input from", claim)
+            
+            
+# Application Interface
+
 option = st.radio("", ["General News", "COVID News"])
 
 if option == "General News":
@@ -119,17 +149,25 @@ if option == "General News":
     predict(url, general_news_inference)
 
 else:
-    url = st.text_input("Please input a url.")
+    covid_options = st.radio("", ["URL", "Claim"])
     
     # Load the model and tokenizer
     covid_inference = keras.models.load_model("code-covid/covid-model-1")
     with open('code-covid/covid1-tokenizer.json') as f:
         data = json.load(f)
         tokenizer = tokenizer_from_json(data)
-    
-    # Predict based on the URL
-    covid_predict(url, covid_inference)
-    
+
+    if covid_options == "URL":
+        covid_url = st.text_input("Please input a covid-related url.")
+   
+        # Predict based on the URL
+        covid_predict(covid_url, covid_inference)
+    else:
+        user_input = st.text_input("Please input a covid-related claim.")
+        
+        # Predict based on the claim
+        covid_claim_pred(user_input, covid_inference)
+
         
 
     
